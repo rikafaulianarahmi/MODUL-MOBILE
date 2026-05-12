8294ce3f40a9312db13d6a9c6db74ba0478a7c7e
rikafaulianarahmi <rika955.brb123@gmail.com>
2026-05-12 21:42:10 +0800

    Add files via upload

---
+++ MODUL 4/XML/app/src/main/java/com/example/modul3xml/MyApplication.kt
@@ -0,0 +1,11 @@
+package com.example.modul3xml
+
+import android.app.Application
+import timber.log.Timber
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
